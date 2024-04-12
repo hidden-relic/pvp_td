e2bd67881b261b59c2b0bd29d0d6d898242571bf
Hidden Relic <the.true.hidden.relic@gmail.com>
2024-04-11 20:49:48 -0500

    fixed the line algorithm, added logging feature for debugging, returns a table of positions

--- tools/path_builder.lua
+++ tools/path_builder.lua
@@ -5,34 +5,31 @@ local PathBuilder =
     paths = {}
 }
 
+-- create a path builder instance:
+-- -- local path = PathBuilder:new({position = origin, tick = game.tick})
+-- get a list of the tile positions in a line:
+-- -- local tiles = path:get_path(destination)
+-- queue a tile in the builder
+-- -- path:add{tick=config.ticks_between_tiles, position={x=tile.x, y=tile.y}}
+
 local config =
 {
     surface = 'oarc',
     path_tile = 'landfill',
-    ticks_between_tiles = 1,
-    origin = {x=0, y=0}
+    ticks_between_tiles = 5,
+    origin = {x=0, y=0},
+    logging = false
+    -- will log "origin xy to destination xy" and all the tile positions between for each new line
 }
 
--- -- position functions to return a position relative to a position
--- local function down(position) return {x=position.x, y=position.y + 1} end
--- local function right(position) return {x=position.x + 1, y=position.y} end
--- local function up(position) return {x=position.x, y=position.y - 1} end
--- local function left(position) return {x=position.x - 1, y=position.y} end
-
 -- returns distance in tiles between 2 positions, will be used to get progress of the paths
-function getDistance(posA, posB)
-    -- Get the length for each of the components x and y
-    local xDist = posB.x - posA.x
-    local yDist = posB.y - posA.y
-
-    return math.sqrt( (xDist ^ 2) + (yDist ^ 2) )
-end
-
--- round a number 'num' to 'dp' decimal places. default is 0 (whole number)
-local function round(num, dp)
-    local mult = 10 ^ (dp or 0)
-    return math.floor(num * mult + 0.5) / mult
-end
+-- function getDistance(posA, posB)
+--     -- Get the length for each of the components x and y
+--     local xDist = posB.x - posA.x
+--     local yDist = posB.y - posA.y
+    
+--     return math.sqrt( (xDist ^ 2) + (yDist ^ 2) )
+-- end
 
 -- create a new path object
 function PathBuilder:new(definition)
@@ -59,38 +56,76 @@ function PathBuilder:update(tick)
     -- get our current instruction
     if tick < action.tick + self.last_tick then return end
     -- is it time to run this instruction yet?
-
+    
     -- perform action
     self.index = self.index + 1
     self.position = action.position
     -- update our position
     local surface = game.surfaces[config.surface]
---    surface.set_tiles{
---        tiles = {name = config.path_tile, position = self.position}
---    }
+    surface.set_tiles{
+        tiles = {name = config.path_tile, position = self.position}
+    }
     -- set our tile to finish our instruction
     self.last_tick = self.last_tick + action.tick
     --update our timer
 end
 
-function PathBuilder:queue_path(position)
+function PathBuilder:get_path(position)
     local origin = config.origin
+    
+    local ret = {}
+    -- collect tile positions in a table
+    
+    if config.logging then
+        game.write_file('path_tiles.lua', 'Path from '..serpent.line(origin)..' to '..serpent.line(position)..'\n', true)
+    end
+    
     local path = PathBuilder:new({position = origin, tick = game.tick})
     -- create a PathBuilder instance
     table.insert(PathBuilder.paths, path)
     -- keep track of instance for on_tick handler
-
+    
     -- bresenham.line usage: success, counter = bresenham.line(ox, oy, ex, ey, callback)
     -- the callback function is passed each tile's position and the current count.
     -- the callback must return true to continue the path,
     -- this is how you can say that you've hit something and need to stop the path.
     -- returns 2 values, a boolean success, and the total tile count number
     -- if no callback is provided, just returns the total tile count number
+
     local success, counter = bresenham.line(origin.x, origin.y, position.x, position.y, function( x, y, counter )
-        path:add{tick=config.ticks_between_tiles, position={x=x, y=y}}
+        -- checking that the line has not grown past our destination
+        if ((position.x >= origin.x) and (x >= position.x)) then
+            if ((position.y >= origin.y) and (y >= position.y))
+            or ((position.y <= origin.y) and (y <= position.y))
+            then
+                -- returning false stops the path
+                return false
+            end
+        end
+        if ((position.x <= origin.x) and (x <= position.x)) then
+            if ((position.y >= origin.y) and (y >= position.y))
+            or ((position.y <= origin.y) and (y <= position.y))
+            then
+                -- returning false stops the path
+                return false
+            end
+        end
+        
+        -- uncomment if creating the path now, 1 tile at a time
+        -- path:add{tick=config.ticks_between_tiles, position={x=round(x), y=round(y)}}
+        
+        table.insert(ret, {x=x, y=y})
+        -- add tile position to table
+        
+        if config.logging then
+            game.write_file('path_tiles.lua', counter..': x'..x..', y'..y..'\n', true)
+        end
+        -- returning true continues the path
         return true
-    end) 
+    end)
     -- counter can be used within the callback above as well to get the count as you go
+    -- return our table of tile positions
+    return ret
 end
 
 local function on_tick()
@@ -117,15 +152,9 @@ PathBuilder.on_init = function()
     -- setup the surface and tile grid
     local s = game.surfaces["oarc"] or game.create_surface('oarc')  -- if running with oarc, use that surface, otherwise create
     -- s.generate_with_lab_tiles = true
+    game.write_file('path_tiles.lua', '')
+    -- reset the log file
     s.always_day = true
 end
-
-commands.add_command('createpath', 'test path creation', function(command)
-    local player = game.players[command.player_index]
-    player.surface.set_tiles{
-        tiles = {name = 'concrete', position = player.position}
-    }
-    PathBuilder:queue_path(player.position)
-end)
 
 return PathBuilder
